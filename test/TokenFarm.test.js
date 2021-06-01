9e053641557d978f73e1fe48dd8e451a15a0020a
Jefferson <jeffarrubla@gmail.com>
2021-06-01 20:29:09 +0000

    Added unstakeTokens (withdraw) and its tests, besides the script for issuing tokens.

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -75,6 +75,32 @@ contract('TokenFarm', ([owner, investor]) => {
 
 			result = await tokenFarm.isStaking(investor);
 			assert.equal(result.toString(), 'true','investor staking status correct after staking');
+
+			// Issue token
+			await tokenFarm.issueTokens({from:owner})
+
+			// Check balances after issuing
+			result = await dappToken.balanceOf(investor);
+			assert.equal(result.toString(), tokens('100'),'investor DApp Token wallet balance after issuing tokens');
+
+			// Ensure that only owner can issue tokens
+			await tokenFarm.issueTokens({from: investor}).should.be.rejected;
+
+			// Unstake tokens
+			await tokenFarm.unstakeTokens({from:investor})
+
+			// Check results after unstaking
+			result = await daiToken.balanceOf(investor);
+			assert.equal(result.toString(), tokens('100'),'investor Mock DAI wallet balance correct after staking');
+
+			result = await daiToken.balanceOf(tokenFarm.address);
+			assert.equal(result.toString(), tokens('0'),'investor Mock DAI wallet balance correct after staking');
+
+			result = await tokenFarm.stakingBalance(investor);
+			assert.equal(result.toString(), tokens('0'),'investor staking balance correct after staking');
+
+			result = await tokenFarm.isStaking(investor);
+			assert.equal(result.toString(), 'false','investor staking status correct after staking');
 		});
 	});
 });
